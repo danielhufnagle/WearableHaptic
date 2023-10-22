11954d3bc8606eca7ee7c9cdd1f2cffa14ee65a2
Daniel Hufnagle <danielhufnagle@Daniels-MacBook-Pro.local>
2023-10-22 00:55:45 -0400

    fixed - kinda

--- PWMShiftRegister.py
+++ PWMShiftRegister.py
@@ -2,48 +2,42 @@
 
 # import dependencies
 import RPi.GPIO as GPIO
-# set pin numbering system to BCM
+from time import sleep
+
+# set pin numbering system
 GPIO.setmode(GPIO.BCM)
 
-# declare and set up data, latch, clock pins
+# setup data, latch, clock pins
 PIN_DATA = 16
 PIN_LATCH = 20
 PIN_CLOCK = 21
-
 GPIO.setup(PIN_DATA, GPIO.OUT)
 GPIO.setup(PIN_LATCH, GPIO.OUT)
 GPIO.setup(PIN_CLOCK, GPIO.OUT)
 
-# turn off warnings
-GPIO.setwarnings(False)
+GPIO.setwarnings = False
 
-# set led pattern (1 is on, 0 is off - be aware this might be "backwards")
-ledpattern = '00100111'
+# set pattern for the LEDs, 1 is on 0 is off
+ledpattern = [1, 1, 1, 1, 1, 1, 1, 1]
 
-# start PWM instance with frequency
-pwm_inst = GPIO.PWM(PIN_DATA, 1000)
-
-GPIO.output(PIN_LATCH, 0)
-
-# light up the LEDs - hopefully
-for x in range(len(ledpattern)):
-    # if the led is to be lit, marked by 1 in ledpattern
-    if ledpattern[x] == "1":
-        # gradually brightens the LED
-        for duty in range(0, 101):
-            pwm_inst.ChangeDutyCycle(duty)
-            sleep(0.01)
-        sleep(0.01)
-        # gradually darkens the LED
-        for duty in range(100, -1, -1):
-            pwm_inst.ChangeDutyCycle(duty)
-            sleep(0.01)
-    # if the led is not to be lit, marked by 0 in ledpattern
-    else:
-        GPIO.output(PIN_DATA, 0) # this might not work
-        # if the code doesn't work then comment out the previous line and uncomment the following line
-        # pwm_inst.ChangeDutyCycle(0)
-    GPIO.output(PIN_CLOCK, 1)
-    GPIO.output(PIN_CLOCK, 0)
-
-GPIO.output(PIN_LATCH, 1)
+# manual pulse width modulation
+while True:
+    try:
+        # light up LEDS according to pattern
+        for i in range(len(ledpattern)):
+            GPIO.output(PIN_DATA, ledpattern[i])
+            GPIO.output(PIN_CLOCK, 1)
+            GPIO.output(PIN_CLOCK, 0)
+        GPIO.output(PIN_LATCH, 1)
+        GPIO.output(PIN_LATCH, 0)
+        # turn off LEDS
+        for i in range(len(ledpattern)):
+            GPIO.output(PIN_DATA, 0)
+            GPIO.output(PIN_CLOCK, 1)
+            GPIO.output(PIN_CLOCK, 0)
+        GPIO.output(PIN_LATCH, 1)
+        GPIO.output(PIN_LATCH, 0)
+        # set sleep period between darkening and lighting... Longer sleep means "dimmer LEDs"
+        sleep(0)
+    except KeyboardInterrupt:
+        break
